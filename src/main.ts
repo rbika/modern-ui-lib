5863ef00fa11d42f8d1c341cab3f2c7a8a8070b8
Rafael Bicalho <rbmbika@gmail.com>
2025-11-16 22:56:36 -0300

    fix(build): export checkbox and switch components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './styles/main.css'
 
-export * from './components/button'
 export * from './components/alert-dialog'
+export * from './components/button'
+export * from './components/checkbox'
+export * from './components/switch'
